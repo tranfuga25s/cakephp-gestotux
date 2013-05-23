7d2d80d48bdfab05fed07ff7d2dd6bcdfc2817a5
tranfuga25s@gmail.com <tranfuga25s>
2013-05-23 20:40:45 -0300

    Actualizada la conexión de base de datos

--- Controller/GestotuxAppController.php
+++ Controller/GestotuxAppController.php
@@ -2,16 +2,16 @@
 App::uses( 'ConnectionManager', 'Model' );
 
 class GestotuxAppController extends AppController {
-	
+
 	public function necesitaConexion() {
 		ConnectionManager::create( 'gestotux',
 		 array(
 		     'datasource' => 'Database/Mysql',
 		     'persistent' => false,
 		     'host' => 'trafu.no-ip.org',
-		     'login' => 'gestotux',
-		     'password' => 'gestotux',
-		     'database' => 'gestotux',
+		     'login' => 'trsisgestotux',
+		     'password' => 'trsisgestotux',
+		     'database' => 'trsis-gestotux',
 		     'prefix' => '',
 		     'encoding' => 'utf8'
 		    )
