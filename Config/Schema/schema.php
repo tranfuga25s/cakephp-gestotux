6734f0029be7bebde74f983173e0ee55fb9c8a23
tranfuga25s <tranfuga25s@gmail.com>
2013-08-05 09:01:09 -0300

    Agregado schema para cobro de sms

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -21,4 +21,17 @@ class GestotuxSchema extends CakeSchema {
         ),
         'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_spanish_ci', 'engine' => 'InnoDB')
     );
+    
+    public $cobro_sms = array(
+        'id_cobro_sms' => array( 'type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary' ),
+        'cliente_id' => array( 'type' => 'integer', 'null' => false, 'default' => null, 'length' => 10 ),
+        'fecha' => array( 'type' => 'date', 'null' => false, 'default' => null ),
+        'costo' => array('type' => 'float', 'null' => true, 'default' => null, 'length' => '10,4' ),
+        'mov_ctacte_id' => array( 'type' => 'integer', 'null' => false, 'default' => 0, 'length' => 10),
+        'indexes' => array(
+            'PRIMARY' => array( 'column' => array( 'id_cobro_sms' ), 'unique' => 1 ),
+            'movctacteunique' => array( 'column' => array( 'mov_ctacte_id' ), 'unique' => 1 )
+        ),
+        'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_spanish_ci', 'engine' => 'InnoDB')
+    );
 }
