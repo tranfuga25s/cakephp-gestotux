202c2eaff27cdebe368e910d4ca40a77609ebed0
tranfuga25s <tranfuga25s@gmail.com>
2013-08-13 17:31:30 -0300

    Agregados metodos para factuar y avisar a la gestión

--- Console/Command/GestotuxShell.php
+++ Console/Command/GestotuxShell.php
@@ -2,7 +2,7 @@
 
 App::uses('CakeEmail', 'Network/Email');
 
-class TurnosShell extends AppShell {
+class GestotuxShell extends AppShell {
 
 	var $uses = array( 'Gestotux.Cliente', 'Gestotux.Ctacte', 'Gestotux.Conteosms' );
 
@@ -27,19 +27,47 @@ class TurnosShell extends AppShell {
         { $this->out( 'El plugin de gestotux no está configurado correctamente. No existe el cliente' ); return; }
         $id_ctacte = $this->Ctacte->obtenerCtacte( $this->Cliente->id );
         if( $id_ctacte === false ) { $this->out( 'No hay asociación entre el cliente y la cuenta corriente' ); return; }
+
+        // Defino en que mes estoy
+        $mes = date( 'j' );
+        $texto_mes = date( 'F' );
         
         // Busco el conteo de mensajes
-        $cant_mesnajes = $this->ConteoSms->buscarConteoMes( $id_cliente, $mes );
+        $cant_mensajes = $this->ConteoSms->buscarConteoMes( $id_cliente, $mes );
         $precio_mensaje = $this->ConteoSms->buscarPrecioSms( $id_cliente, $mes );
-        $importe_total = $cant_mesnajes * $precio_mensaje;
-        $texto = "Cobro de ".$cant_mesnajes." mensajes sms utilizados - Mes: ".$texto_mes;
+        $texto = "Cobro de mensajes sms utilizados - Mes: ".$texto_mes;
         
+        $items = array( array( 'cantidad' => $cant_mensajes, 'texto' => $texto, 'precio_unitario' => $precio_mensaje ) );
+        if( !$this->Factura->agregarFacturaSms( $id_cliente, $items ) ) {
+            $this->out( 'No se pudo agregar correctamente la factura de los mensajes' );
+            return;
+        }
+        $this->out( 'Factura guardada correctamente' );
+        $id_factura = $this->Factura->id;
+        $num_factura = $this->Factura->obtenerNumeroComprobante();
         
+        $datos = array(
+            'factura' => array( 'id' => $id_factura, 'numero' => $num_factura ),
+            'sms' => array(
+                'cantidad' => $cant_mensajes,
+                'precio_mensaje' => $precio_mensaje,
+                'resumen' => $this->ConteoSms->obtenerListado( $id_cliente, $mes )                    
+            )
+        );
+        $template = 'Gestotux.CobroSms';
+        $layout = 'cobranza';
         
-        
-        
-        
-        
+        // Envio el correo electronico correspondiente
+        $email = new CakeEmail();
+        $email->template( $template, $layout )
+              ->emailFormat( 'both' )
+              ->from( 'gestotux@gestotux.com' )
+              ->to( 'esteban.zeller@gmail.com' )
+              ->subject( 'Nueva cobranza de sms' )
+              ->viewVars( $datos )
+              ->send();
+        $this->out( 'Mensaje enviado a cobranzas' );
+        return;        
     }
 }
 ?>
