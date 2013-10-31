e9ae2f464a53582a7aa2c8d5e7231e3a7fa341f4
tranfuga25s <tranfuga25s@gmail.com>
2013-10-31 12:01:43 -0300

    Agregada consola de actualizacion

---
+++ Console/Command/SyncronizeShell.php
@@ -0,0 +1,28 @@
+<?php
+//App::uses('CakeEmail', 'Network/Email');
+
+class SyncronizeShell extends AppShell {
+
+	//var $uses = array( 'Aviso' );
+	
+	private $tablas = array(
+	   'clientes',
+	   'ctacte'
+    );
+	
+	public function main() {
+        $this->out('Opciones de la consola de sincronizacion:');
+        $this->out('');
+        $this->out('Comandos:');
+        $this->out(' - sincronizarTodo: Sincroniza todas las tablas que esten declardas.');
+        $this->out(' - listarTablas: Lista las tablas que se van a sincronizar.' );
+    }
+    
+    public function listarTablas() {
+        $this->out( "Lista de tablas actualizables");
+        foreach( $this->tablas as $tabla ) {
+            $this->out( " - ".$tabla );
+        }
+    }
+}
+?>
